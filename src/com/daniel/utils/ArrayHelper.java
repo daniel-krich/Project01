18a5dabd0ac6a915af976c3f29e69f125c4c8ffa
Daniel <danielkrich147@gmail.com>
2026-04-17 22:16:08 +0300

    finalize

--- src/com/daniel/utils/ArrayHelper.java
+++ src/com/daniel/utils/ArrayHelper.java
@@ -3,9 +3,9 @@ package com.daniel.utils;
 public class ArrayHelper {
     public static Object[] resizeArray(Object[] arr, int size) {
         Object[] array = new Object[size];
-            for(int i = 0; i < size; i++) {
-                array[i] = arr[i];
-            }
+        for(int i = 0; i < size; i++) {
+            array[i] = arr[i];
+        }
         return array;
     }
 }
